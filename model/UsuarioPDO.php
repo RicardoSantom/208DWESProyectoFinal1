3fa46abd25070869efa75e55045b1f8168e379de
RicardoSantom <richard1978_1@hotmail.com>
2023-01-30 10:40:35 +0100

    Actualización UsuarioPDO, corregido fallo devolución null.

--- model/UsuarioPDO.php
+++ model/UsuarioPDO.php
@@ -29,22 +29,22 @@ class UsuarioPDO implements UsuarioDB {
                         $oDatos->T01_DescUsuario, $oDatos->T01_NumConexiones, $oDatos->T01_FechaHoraUltimaConexion,
                         $oDatos->T01_Perfil, $oDatos->T01_ImagenUsuario);
                 return $oUsuario;
-            } else { //Si no existe, devuelve false.
+            } /*else { //Si no existe, devuelve false.
                 return false;
-            }
+            }*/
         } catch (Exception $ex) {
             echo $ex->getMessage();
         }
     }
 
     public static function registrarUltimaConexion($oUsuario) {
-        $setNumConexiones = $oUsuario->setNumConexiones($oUsuario->getNumConexiones() + 1);
-        $sQueryActualizacion = <<<query
+        $oUsuario->setNumConexiones($oUsuario->getNumConexiones() + 1);
+        $SQLActualizacionNumConexiones = <<<query
               UPDATE T01_Usuario SET T01_NumConexiones=T01_NumConexiones+1,T01_FechaHoraUltimaConexion=now()
               WHERE T01_CodUsuario="{$oUsuario->getCodUsuario()}";
               query;
-        DBPDO::ejecutarConsulta($sQueryActualizacion);
-        return $setNumConexiones;
+        DBPDO::ejecutarConsulta($SQLActualizacionNumConexiones);
+        return $oUsuario;
     }
 
     public static function altaUsuario() {
